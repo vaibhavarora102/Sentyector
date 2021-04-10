d717583e91e9b237474fd54f49a3e8b4e7fb9d84
sairish2001 <sairish2001@gmail.com>
2021-04-10 16:54:00 +0530

    heroku requirements specified

--- SentyectorAPI/test.py
+++ SentyectorAPI/test.py
@@ -2,14 +2,14 @@ import requests
 import json
 
 # ---for prediciton---
-url = 'http://127.0.0.1:8000/api/predemotions/'
+url = 'https://sentyectorapi.herokuapp.com/api/predemotions/'
 data = {'textopredict': "You're just too good to be true can't take my eyes off you you'd be like heaven to touch I wanna hold you so much I love you baby"}
 data = json.dumps(data)
 r = requests.post(url, data = data)
 print(r.text)
 
 # ---for audiototext---
-url = 'http://127.0.0.1:8000/api/audiochunktotext/'
+url = 'https://sentyectorapi.herokuapp.com/api/audiochunktotext/'
 files={'file':open(r'C:\Users\Sairish\Downloads\myrecord.wav','rb')}
 r = requests.post(url, files = files)
 print(r.text)
